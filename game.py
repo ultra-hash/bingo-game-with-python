fb1d37ab88b421583864f66ed7ad8d18d60ed236
ultra-hash <78792323+ultra-hash@users.noreply.github.com>
2021-11-19 14:58:49 +0530

    Update game.py

--- game.py
+++ game.py
@@ -23,12 +23,14 @@
 
 
 """
+
+
 import random, time
 
 def main():
     
-    player1 = computer('swamy')
-    player2 = computer('computer')
+    player1 = player('swamy')
+    player2 = player('damu')
     engine = game(player1, player2)
 
     player1.board = board()
@@ -37,13 +39,16 @@ def main():
     #player1.board.print_board()
     while len(engine.set) != 25 and engine.game_not_over:
         next = engine.who_is_next()
-        next.board.print_board()
+        if isinstance(player1, player) and isinstance(player2, player):
+            next.board.print_board()
+        else:
+            player1.board.print_board()
         engine.strike(next,engine)
         winner = engine.gamewinner
     print()
     print('Printing Both Players Boards & Score')
     print(' Player1'.center(15," ")+'   '+' player2'.center(15," "))
-    print(f' score : {player1.name}'.center(15," ")+'   '+f' score : {player2.name}'.center(15," "))
+    print(f' name : {player1.name}'.center(15," ")+'   '+f' name : {player2.name}'.center(15," "))
     print(f' score : {player1.score}'.center(15," ")+'   '+f' score : {player2.score}'.center(15," "))
 
     for i in range(5):
@@ -53,6 +58,18 @@ def main():
 
     return (player1.name,player2.name,winner)
 
+def max_games(count):
+    (c,p1,p2) = (0,0,0)
+    while c != count:
+        (player1,player2,winner) = main()
+        if winner == player1:
+            p1+=1
+        elif winner == player2:
+            p2+=1
+        c+=1
+        #print(winner)
+    print(player1,' wins :', p1 ,player2,'wins :',p2)
+
 class board:
     def __init__(self):
         self.boardsize = 25
@@ -60,6 +77,7 @@ class board:
         random.shuffle(self.board)
     
     def print_board(self):
+        print()
         for i in range(5):
             print('|'+ '|'.join(self.board[i*5:(i*5)+5])+ '|')
 
@@ -127,7 +145,7 @@ class game:
             #print(i)
             if all(i):
                 player.score+=1
-        print('name :',player.name, 'score :', player.score)
+        print('name :',player.name, '| score :', player.score)
         
         return self.winner(player)
 
@@ -173,13 +191,35 @@ class computer:
 
 
 
-(c,p1,p2) = (0,0,0)
-while c != 100:
-    (player1,player2,winner) = main()
-    if winner == player1:
-        p1+=1
-    elif winner == player2:
-        p2+=1
-    c+=1
-    print(winner)
-print('player1 wins :', p1 ,'player2 :',p2)
+
+class supercomputer:
+    def __init__(self,name):
+        self.name = name
+        self.score = 0
+
+    def c_input(self,gameobj):
+        self.cinput = random.choice([str(x) for x in range(1,26) if x not in gameobj.set]).rjust(2, ' ')
+        while self.cinput in gameobj.set or self.cinput not in self.board.board:
+            self.cinput = random.choice([str(x) for x in range(1,26) if x not in gameobj.set]).rjust(2, ' ')
+
+        print(self.name,'choose', self.cinput)
+        return self.cinput
+
+    def state(self,gameobj):
+        self.init_board = [self.board.board[i*5:(i*5)+ 5] for i in range(5)]
+        '''
+            if 1 star or less presnet in borad try star in corners and mid point
+            if more than 1 star identify star's location and put a start striking max star's 
+        '''
+    def checks(self):
+        self.virtual_board = self.init_board
+        #horizontal check
+        for i in range(5):
+            for j in range(5):
+                if self.virtual_board[i][j] == '*':
+                    location = 
+
+
+
+#max_games(100)
+main()
